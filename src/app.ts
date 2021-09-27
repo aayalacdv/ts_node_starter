487f5be4ae171f51762350c65420a70f5974081a
aayalacdv <aayalacdv2000@gamil.com>
2021-09-27 22:26:41 +0200

    added cors

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,13 @@ import config from 'config';
 import express from 'express'; 
 import routes from '../routes/routes';
 import connect from './connection/connect';
+import cors from 'cors'; 
 
 const host : string = config.get("host") as string; 
 const port : number = config.get("port") as number; 
 
 const app = express(); 
+app.use(cors()); 
 app.use( express.json()); 
 app.use( express.urlencoded({ extended: false})); 
 
