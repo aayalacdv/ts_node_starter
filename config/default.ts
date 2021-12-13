8d20d438ba2c277878c3ac15d0d757bf7b458bcb
aayalacdv <aayalacdv2000@gmail.com>
2021-12-13 17:14:34 +0100

    added Docker files

--- config/default.ts
+++ config/default.ts
@@ -1,8 +1,8 @@
 export default {
     //host y puerto donde correrá el servidor
-    host: 'localhost', 
+    host: '0.0.0.0',
     //URI de conexión a la base de datos
     port: 8080, 
-    dbUri: 'mongodb://localhost:27017/seminario',
+    dbUri: 'mongodb://mongo:27017/seminario',
 
 }
